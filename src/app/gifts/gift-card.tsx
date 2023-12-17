c9a34bc295f9f7d945011ee29011704ac573cfe3
Henrique Miranda <enrque.ns@gmail.com>
2023-12-17 15:50:32 -0300

    style: gift card

---
+++ src/app/gifts/gift-card.tsx
@@ -0,0 +1,36 @@
+import { CheckIcon } from "@radix-ui/react-icons";
+
+import { Gift } from "~/types/gift";
+import { Button } from "~/ui/button";
+import { formatCentsToCurrency } from "~/utils/format-currency";
+
+type Props = {
+  gift: Gift;
+};
+
+export function GiftCard({ gift }: Props) {
+  const [image] = gift.images;
+
+  return (
+    <div
+      key={gift.slug}
+      className="grow flex-1 group border-4 p-4 rounded-xl min-w-[220px] max-w-lg border-gray-200 w-full hover:border-primary-500 transition-all"
+    >
+      <img
+        alt={image?.fields.title}
+        src={image?.fields.file.url}
+        className="w-full h-32 object-cover rounded-lg mb-4 transition-all group-hover:-translate-y-2 group-hover:scale-110"
+      />
+
+      <span className="text-lg block">{gift.name}</span>
+      <div className="flex w-full items-center justify-between">
+        <span className="font-bold text-2xl text-primary-500">
+          {formatCentsToCurrency(gift.priceInCents)}
+        </span>
+        <Button className="bg-primary-500 text-white rounded-lg w-10 p-0">
+          <CheckIcon className="stroke-white w-8 h-8 p-1.5" />
+        </Button>
+      </div>
+    </div>
+  );
+}
--- src/app/gifts/page.tsx
+++ src/app/gifts/page.tsx
@@ -1,8 +1,6 @@
-import { PlusIcon } from "@radix-ui/react-icons";
-import Link from "next/link";
-import { formatCentsToCurrency } from "~/utils/format-currency";
 import { Pagination } from "../../ui/pagination";
 import { getGifts } from "./get-gifts";
+import { GiftCard } from "./gift-card";
 import { GiftFilter } from "./gift-filter";
 import { Header } from "./header";
 
@@ -25,36 +23,12 @@ export default async function GiftsPage({ searchParams }: Props) {
     <div className="flex flex-col overflow-hidden">
       <Header />
 
-      <main className="p-8">
+      <main className="p-8 max-w-7xl w-full mx-auto">
         <GiftFilter />
 
-        <div className="flex">
+        <div className="flex justify-start items-start flex-wrap gap-6">
           {gifts.items.map((g) => (
-            <Link
-              key={g.slug}
-              href={`/gifts/${g.slug}`}
-              className="border-4 p-4 rounded-xl border-gray-200 mr-auto min-w-[48] w-full max-w-xs"
-            >
-              {g.images.map((i) => (
-                <img
-                  alt={i.fields.title}
-                  src={i.fields.file.url}
-                  className="w-full h-32 object-cover rounded-lg mb-4"
-                />
-              ))}
-              <span className="text-lg block">{g.name}</span>
-              <div className="flex w-full items-center justify-between">
-                <span className="font-bold text-xl text-primary-500">
-                  {formatCentsToCurrency(g.priceInCents)}
-                </span>
-                <button
-                  type="button"
-                  className="bg-primary-500 text-white rounded-lg"
-                >
-                  <PlusIcon className="stroke-white w-8 h-8 p-1.5" />
-                </button>
-              </div>
-            </Link>
+            <GiftCard key={g.slug} gift={g} />
           ))}
         </div>
 
